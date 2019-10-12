d1de9c46022c3426ada2394e76890e81bf2d859f
Elena Czubiak <elenaczubiak@gmail.com>
2019-10-12 11:46:07 -0700

    Downgrade sass-loader to 7.0.0 to work with current vue-cli, add variable stylesheet for veutify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,5 +6,18 @@ Vue.use(Vuetify);
 export default new Vuetify({
   icons: {
     iconfont: "mdi"
+  },
+  theme: {
+    options: {
+      customProperties: true
+    },
+    themes: {
+      light: {
+        primary: "#00CEE6",
+        secondary: "#1DE9B6",
+        accent: "#F44336",
+        error: "#F44336"
+      }
+    }
   }
 });
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "Quetzal Translate",
+    themeColor: "#00CEE6",
+    msTileColor: "#00CEE6",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "default"
+  },
+  css: {
+    loaderOptions: {
+      // sass: {
+      //   // Here we can specify the older indent syntax formatted SASS
+      //   // Note that there is *not* a semicolon at the end of the below line
+      //   data: `@import "@/styles/_variables.sass"`
+      // },
+      scss: {
+        // Here we can use the newer SCSS flavor of Sass.
+        // Note that there *is* a semicolon at the end of the below line
+        // data: `@import "@/styles/variables.scss";`
+      }
+    }
+  }
 };
