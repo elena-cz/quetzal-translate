e95a01ce583737544188d802496f40b38dca3290
Elena Czubiak <elenaczubiak@gmail.com>
2019-10-23 15:52:15 -0700

    Add Admin grid

--- src/views/Admin-View.vue
+++ src/views/Admin-View.vue
@@ -43,9 +43,53 @@ export default {
 </script>
 
 <template>
-  <div>{{page}}</div>
+  <div class="app-container">
+    <v-app-bar app flat color="transparent">
+      <v-toolbar-title class="headline white--text">
+        <span>Quetzal Admin</span>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="admin-grid">
+        <aside class="sidebar">sidebar</aside>
+        <div class="main">main</div>
+      </v-container>
+    </v-content>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-// @import '@/design/colors.scss';
+.admin-grid {
+  display: grid;
+  grid-template-columns: 230px auto;
+  grid-template-rows: auto;
+  grid-template-areas: 'sidebar main';
+  grid-gap: 1rem;
+  align-content: flex-start;
+  min-height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 1rem;
+}
+
+.sidebar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: calc(230px + 1rem);
+  height: 100%;
+  max-height: 100%;
+  padding: 1rem;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+
+.main {
+  grid-area: main;
+  width: 100%;
+}
 </style>
