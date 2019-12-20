43a44c13350de66ec2aa88bb378f26cf850190b8
Elena Czubiak <elenaczubiak@gmail.com>
2019-12-19 21:04:28 -0800

    Add about content

--- src/views/About-View.vue
+++ src/views/About-View.vue
@@ -1,39 +1,8 @@
 <script>
-import { mapState, mapGetters, mapActions } from 'vuex';
-import Layout from '@/components/Layout.vue';
-
 export default {
   name: 'About-View',
 
-  components: {
-    Layout,
-  },
-
-  props: {
-    // thing: {
-    //   type: Array,
-    //   required: true,
-    // },
-  },
-
-  data() {
-    return {};
-  },
-
-  computed: {
-    // ...mapState('module', [
-    //   'foo',
-    // ]),
-    // ...mapGetters('module', [
-    //   'foo',
-    // ]),
-    // property() {},
-  },
-
   methods: {
-    // ...mapActions('module', [
-    //   'foo',
-    // ]),
     showSnack() {
       this.$store.dispatch('ui/updateSnack', {
         text: 'Test snack',
@@ -49,34 +18,56 @@ export default {
 </script>
 
 <template>
-  <div>
-    <!-- <Layout page-title="About">
-    <template v-slot:main-content>-->
-    <v-btn rounded color="primary" @click.stop="showSnack">Show Snack</v-btn>
-    <ol>
-      <li>Good day!</li>
-      <li>Please sit here.</li>
-      <li>I am your dentist today</li>
-      <li>Are you allergic to any medicines?</li>
-      <li>Where does it hurt?</li>
-      <li>When does it hurt?</li>
-      <li>All the time?</li>
-      <li>Or now and then?</li>
-      <li>Does the cold water or air hurt you?</li>
-      <li>Does it wake you up at night?</li>
-      <li>Does it hurt when you bite?</li>
-      <li>We need to fix your broken tooth</li>
-      <li>We need to take x-rays.</li>
-      <li>We need to remove this tooth.</li>
-      <li>You have some cavities that we must treat.</li>
-      <li>We need to fill ____ teeth. (show fingers)</li>
-      <li>We need to extract ____ teeth. (show fingers)</li>
-      <li>This tooth cannot be repaired. We must extract it.</li>
-      <li>This tooth is loose.</li>
-    </ol>
-    <!-- </template> -->
-    <!-- </Layout> -->
-  </div>
+  <v-container class="pa-0 pb-6">
+    <h3 class="mb-3">Quetzal Translate</h3>
+
+    <p>
+      Our mission is to help care providers compassionately communicate with
+      clients in their local language, focusing on smaller languages that are
+      underserved by traditional translation services.
+    </p>
+
+    <p>
+      Email us at
+      <a href="mailto:hello@quetzal.app" target="_blank">hello@quetzal.app</a>
+    </p>
+
+    <h3 class="mb-3">Our story</h3>
+
+    <p>
+      Imagine going to the dentist, possibly for the first time in your life,
+      and not being able to communicate or understand. In 2018, we joined the
+      <a href="http://www.internationalhealthemissaries.org/" target="_blank"
+        >International Health Emissaries</a
+      >
+      on their annual trip to Guatemala to provide dental care to children in
+      the Río Dulce area. We learned that many of the Q’eqchi’-speaking children
+      we were serving were in just this position. Guatemala is home to 24 local
+      languages that are spoken by millions of people.
+    </p>
+
+    <p>
+      With our skills in languages (Mindy) and design and programming (Elena) we
+      knew we had the tools to do something. The next year, we built an app with
+      dental phrases in Spanish and Q’eqchi’. For the translations and audio
+      recordings, we were lucky to connect with René Corado, an ambassador for
+      Peace in Guatemala and founder of the
+      <a href="https://www.ellustradorfoundation.org/" target="_blank"
+        >El Lustrador Foundation</a
+      >, and Manuel Tiul, a teacher in Punta Arenas, Río Dulce.
+    </p>
+
+    <p>
+      Our project is ready to grow to other languages and subject areas with
+      help from volunteers. If you have any comments, suggestions, or want to
+      get involved, write to us at
+      <a href="mailto:hello@quetzal.app" target="_blank">hello@quetzal.app</a>.
+    </p>
+
+    <p>B'anyox!</p>
+
+    <p>- Mindy & Elena Czubiak</p>
+  </v-container>
 </template>
 
 <style lang="scss" scoped>
